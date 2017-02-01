5668e1147b3e65438ed603c1ea59ebbd6ccbd0c4
Zuliang Zhao <mrzzl1987@gmail.com>
2017-01-31 23:57:01 -0500

    chapter 5

---
+++ Chapter 5/optimization.py
@@ -0,0 +1,24 @@
+import time
+import random
+import math
+
+people = [('Seymour', 'BOS'), ('Franny', 'DAL'), ('Zooey', 'CAK'), ('Walt', 'MIA'), ('Buddy', 'ORD'), ('Les', 'OMA')]
+destination = 'LGA'
+flights = {}
+for line in file('schedule.txt'):
+    origin,dest,depart,arrive,price = line.strip().split(',')
+    flights.setdefault((origin,dest),[])
+
+    flights[(origin,dest)].append((depart,arrive,int(price)))
+
+def getMinutes(t):
+    x=time.strftime(t,'%H:%M')
+    return x[3]*60+x[4]
+
+def printSchedule(r):
+    for d in range(len(r)/2):
+        name=people[d][0]
+        origin=people[d][1]
+        out = flights[(origin,destination)][r[2*d]]
+        ret = flights[(destination,origin)][r[2*d+1]]
+        print '%10s%10s %5s-%5s $%3s %5s-%5s $%3s' % (name,origin,out[0],out[1])
