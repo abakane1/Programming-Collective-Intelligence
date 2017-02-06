dfabfca088966f957c321821cf4d7518692d3753
Zuliang Zhao <mrzzl1987@gmail.com>
2017-02-06 16:53:29 -0500

    chapter 5 page 89 :description

--- Chapter 5/run.py
+++ Chapter 5/run.py
@@ -1,3 +1,12 @@
 import optimization
-s=[1,4,3,2,7,3,6,3,2,4,5,3]
-print optimization.printSchedule(s)
+
+#s=[1,4,3,2,7,3,6,3,2,4,5,3]
+#print optimization.schedulecost(s)
+
+domain = [(0,9)]*(len(optimization.people)*2)
+# s = optimization.randomoptimize(domain,optimization.schedulecost)
+# s = optimization.hillclimb(domain,optimization.schedulecost)
+#s = optimization.annealingoptimize(domain,optimization.schedulecost)
+s = optimization.geneticoptimize(domain,optimization.schedulecost)
+# print optimization.schedulecost(s)
+optimization.printSchedule(s)
