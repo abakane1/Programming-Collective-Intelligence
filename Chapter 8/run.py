d597b0064e893c4921e12b6e7021a25c978ce2f6
Zuliang Zhao <mrzzl1987@gmail.com>
2017-02-20 10:42:29 -0500

    chapter :8 Weighted kNN

--- Chapter 8/run.py
+++ Chapter 8/run.py
@@ -1,10 +1,22 @@
 import numpredict
 
-print numpredict.winpreice(95.0,3.0)
+# print numpredict.winpreice(95.0,3.0)
 
-print numpredict.winpreice(95.0,8.0)
+# print numpredict.winpreice(95.0,8.0)
 
-print numpredict.winpreice(99.0,1.0)
+# print numpredict.winpreice(99.0,1.0)
 data = numpredict.winesetdefaut()
-print data[0]
-print data[1]
+# print data[0]
+# print data[1]
+
+# print numpredict.euclidean(data[0]['input'],data[1]['input'])
+# print numpredict.knnestimate(data,(99.0,3.0))
+# print numpredict.subtractweight(0.1)
+# print numpredict.inverseweight(0.1)
+# print numpredict.gaussian(0.1)
+# print numpredict.gaussian(1.0)
+# print numpredict.subtractweight(1)
+# print numpredict.inverseweight(1)
+# print numpredict.gaussian(3.0)
+
+print numpredict.weightedknn(data,(99.0,5.0))
