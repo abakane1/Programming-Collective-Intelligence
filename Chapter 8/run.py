73a36f04a6895ce99c4526cd1d660c4a3dd930a2
Zuliang Zhao <mrzzl1987@gmail.com>
2017-02-20 07:16:51 -0500

    data

---
+++ Chapter 8/run.py
@@ -0,0 +1,10 @@
+import numpredict
+
+print numpredict.winpreice(95.0,3.0)
+
+print numpredict.winpreice(95.0,8.0)
+
+print numpredict.winpreice(99.0,1.0)
+data = numpredict.winesetdefaut()
+print data[0]
+print data[1]
